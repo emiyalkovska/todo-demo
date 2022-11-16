330919d8b0af22b47436ce23b7c221e992aa5736
emiyalkovska <elena@whiletrue.com>
2022-11-16 15:09:46 +0100

    Project structure

---
+++ src/test/java/com/whiletrue/tododemo/TodoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiletrue.tododemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
