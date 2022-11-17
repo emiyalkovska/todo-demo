c8c82d636ce547310cbd9539911b55df35ee4b37
emiyalkovska <em6841@student.uni-lj.si>
2022-11-17 20:48:46 +0100

    fix for Instant

--- src/main/java/com/whiletrue/tododemo/dto/TaskResponse.java
+++ src/main/java/com/whiletrue/tododemo/dto/TaskResponse.java
@@ -2,6 +2,7 @@ package com.whiletrue.tododemo.dto;
 
 import lombok.Data;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 @Data
@@ -10,7 +11,7 @@ public class TaskResponse {
     private Long id;
     private String name;
     private String description;
-    private LocalDateTime dueDateTime;
+    private Instant dueDateTime;
     private String createdBy;
     private String assignedTo;
     private boolean completed;
